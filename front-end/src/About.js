a41fb994666cb6f4487f0e1e6cc2e744839895cf
reyhanquayum <rrq2003@nyu.edu>
2024-02-14 16:28:31 -0500

    about page

--- front-end/src/About.js
+++ front-end/src/About.js
@@ -1,17 +1,32 @@
-import './About.css'; // Import the corresponding CSS for styling
-import meImage from './img/me.jpg'; // Import the image
-
+import './About.css'
+import meImage from './img/me.jpg'
 
 function About() {
   return (
     <div className="About">
-      <h1><span className="about-name">Reyhan Quayum</span></h1>
-      <p>I am a third year Computer Science & Mathematics student at New York University interested in gaining experience in the blockchain space and exploring the intersection between business and software, and I am also passionate about developing open-source and accessible projects in the areas of assistive technology and other social impact applications. I am most skilled in Java and am proficient in Python as well as C++ and C. I have worked extensively in Information Technology during my time as a student at NYU. 
-        <br></br><strong>Nah, I'd win - Spongebob</strong></p>
-      <br></br>
-      <img src={meImage} alt="Me" className="about-image"/>
+      <h1>
+        <span className="about-name">Reyhan Quayum</span>
+      </h1>
+      <img src={meImage} alt="Reyhan Quayum" className="about-image" />
+      <p>
+        I am a third year Computer Science & Mathematics student at New York
+        University interested in gaining experience in the blockchain space and
+        exploring the intersection between business and software. I am
+        passionate about developing open-source and accessible projects in the
+        areas of financial technology and other social impact applications.
+      </p>
+
+      <p>
+        I am most skilled in Java and am proficient in Python as well as C++ and
+        C. I have worked extensively in Information Technology during my time as
+        a student at NYU.
+      </p>
+
+      <p>Also, I love to watch the sun set on a grateful world. But before I go, here's a quote from an inspirational mentor of mine. </p>
+
+      <blockquote>"Nah, I'd win" - Spongebob</blockquote>
     </div>
-  );
+  )
 }
 
-export default About;
+export default About
